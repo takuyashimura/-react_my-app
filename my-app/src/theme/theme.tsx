f574621a0be0835ea5d78b078cfc4dcf6af5a9d0
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-04-19 15:41:41 +0900

    初回コミット

---
+++ my-app/src/theme/theme.tsx
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    styles: {
+        global: {
+            body: {
+                color: "gray.800",
+            },
+        },
+    },
+});
+export default theme;
