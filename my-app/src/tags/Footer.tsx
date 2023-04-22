c42e1eef0fff70e65ecfe86be36c1450cf3c31ff
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-04-22 14:40:24 +0900

    軽微な修正

--- my-app/src/tags/Footer.tsx
+++ my-app/src/tags/Footer.tsx
@@ -1,17 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react';
 
 export const FooterTagu = (props: any) => {
-    return (
-        <Box
-            bg={"gray.100"}
-            display={"flex"}
-            position={"fixed"}
-            width={"100%"}
-            bottom={0}
-            height={"50px"}
-            {...props}
-        >
-            {props.children}
-        </Box>
-    );
+  return (
+    <Box
+      bg={'red.100'}
+      display={'flex'}
+      position={'fixed'}
+      width={'100%'}
+      bottom={0}
+      height={'50px'}
+      {...props}
+    >
+      {props.children}
+    </Box>
+  );
 };
