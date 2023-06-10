2fcf4037faa6b6288f2c37ba2cec313223247998
takuyashimura <1shi9mu9ra6.0ta9ku1ya3@gmail.com>
2023-06-10 21:44:45 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,6 @@ import axios from 'axios';
 import ReactDOM from 'react-dom';
 import { ReactNode } from 'react';
 
-//
-
 // axios.defaults.baseURL = 'http://localhost:8888/';
 // axios.defaults.baseURL = 'http://43.207.34.41/';
 axios.defaults.baseURL = 'https://snowpanda2.sakura.ne.jp/';
